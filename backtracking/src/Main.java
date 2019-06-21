6fbbfc7480189ded66d575bf8790fa8a4eff2786
stefaneduard-deaconu <stefaneduard.deaconu@gmail.com>
2019-06-21 18:06:43 +0300

    done with the basic Lee

--- backtracking/src/Main.java
+++ backtracking/src/Main.java
@@ -3,17 +3,19 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class Main {
+
     public static void main(String args[]) {
         Stack<Integer> stack = new Stack<Integer>();
 
-        stack.push(-1);
+
         Scanner sc = new Scanner(System.in);
         Integer setLength, subsetLength;
         System.out.print("Cardinal of the set: ");
         setLength = sc.nextInt();
         System.out.print("Cardinal of the arrangements: ");
         subsetLength = sc.nextInt();
-        //
+        // backtracking
+        stack.push(-1);
         while(!stack.empty()) {
             // next possible index:
             if (stack.peek() < setLength - 1)
@@ -24,9 +26,11 @@ public class Main {
             }
             // we check if the current element already is in the stack:
             if (stack.indexOf(stack.peek()) != stack.size() - 1) {
+                // and if it is, we continue as to set the next element
                 continue;
             }
             else {
+                // if we have obtained a complete solution:
                 if (stack.size() == subsetLength) {
                     stack.forEach(
                             (element) -> System.out.print(element.toString() + " ")
